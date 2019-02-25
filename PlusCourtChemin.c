87cf8fe64f538fc29a983da0e7bf3f62872be936
IdrissAl <36738007+IdrissAl@users.noreply.github.com>
2019-02-25 18:20:52 +0100

    Create PlusCourtChemin.c

---
+++ PlusCourtChemin.c
@@ -0,0 +1,32 @@
+Site ** PlusCourtChemin( Site ** sites , taille, Site depart){
+		double **mat= matrice(sites, taille);
+		int cn=1; // 1 pour culturel et -1 pour naturel
+		int  nbSites=0;
+		float temps=504;
+		float temps_retour_depart;
+		int nbPoints=0;
+		int nbPays;
+		char pays[MAX_PAYS];
+		Site ** Itineraire;
+		while( temps>6+temps_départ_site){
+			for(int i=0; i<taille;i++){
+				if((mat) &&(cn){
+					
+					cn=-cn;
+				}
+				if(sites[i]->enDanger){
+					nbpoints+=3;
+				}
+				if(sites[i]->pays!=pays){
+					nbPays++;
+				}
+				nbSites++;
+				pays=sites[i]->pays;
+				if(sites[i]->enDanger==0){
+					nbPoints+=1;
+				}
+				temps_retour_depart=80*distance(
+		}
+	}
+	nbPoints+= 2*nbPays; 
+		
